9ba25327016bf9158e545bb8ef1a0140a8d4a0f3
KalitaAS <146657940+KalitaAS@users.noreply.github.com>
2025-03-15 10:10:11 +0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -15,7 +15,7 @@ document.addEventListener('scroll', () => {
 document.addEventListener('scroll', () => {
     const photos = document.querySelectorAll('.photo-gallery img');
     photos.forEach(photo => {
-        const speed = 0.5;
+        const speed = 0.3;
         const yPos = -(window.pageYOffset * speed);
         photo.style.transform = `translateY(${yPos}px)`;
     });
